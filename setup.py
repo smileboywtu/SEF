7b253487ff8ce2316ffc2bf04058365064cbbd99
smileboywtu <smileboywtu@gmail.com>
2016-05-07 14:38:14 +0800

    add the setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+from SEF import __version__
+
+
+setup(
+    name='SEF',
+    version=__version__,
+    packages=['SEF',],
+    description='symmetry data encryption based on fountain code',
+    author='smileboywtu',
+    author_email='smileboywtu@gmail.com',
+    url='https://github.com/smileboywtu/SEF',
+    lisence='Apache2'
+)
